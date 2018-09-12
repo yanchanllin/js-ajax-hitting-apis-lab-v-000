853364b747efedf459d5710002e5c19e9b290228
YanC Lin <yonalin1@gmail.com>
2018-09-12 20:06:35 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ function getRepositories() {
 req.open("GET", `https://api.github.com/users/${username}/repos`)
 req.send()
 } 
+
+function getCommits(el) {
+  const repo = el.dataset.repository
+  const username = el.dataset.username
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", 'https://api.github.com/repos/' + username + '/' + repo + '/commits')
+  req.send()
+}
