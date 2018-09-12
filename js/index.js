3b5c8d6a309dc9276df12bdce9eeb530e327c673
YanC Lin <yonalin1@gmail.com>
2018-09-12 20:06:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // your code here
+function getRepositories() {
+ var username = document.getElementById("username").value
+ const req = new XMLHttpRequest()
+ req.addEventListener("load", displayRepositories);
+req.open("GET", `https://api.github.com/users/${username}/repos`)
+req.send()
+} 
